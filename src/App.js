3962e3260143ea1645a23a292f5a40911effd5dd
FrameworkTools <155712696+JPark99-7@users.noreply.github.com>
2024-05-09 00:47:21 +0700

    route and pages set-up

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-
-
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import ForgetPassword from "./pages/ForgetPassword";
 function App() {
   return (
-    <div className='text-2xl'>Hello world</div>
+    <>
+      <Router>
+        <Routes>
+            <Route path='/' element = {<Home/>}/>
+            <Route path='/profile' element = {<Profile/>}/>
+            <Route path='/signin' element = {<SignIn/>}/>
+            <Route path='/forgetpassword' element = {<ForgetPassword/>}/>
+        </Routes>
+      </Router>
+    
+    </>
   );
 }
 
